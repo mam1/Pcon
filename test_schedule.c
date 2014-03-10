96c378fb26b863ab749e6fc65f7a10d403738a83
Mark Mara <markmara@cornell.edu>
2014-03-10 11:04:14 -0400

    add test project for schedule

---
+++ test_schedule.c
@@ -0,0 +1,16 @@
+/**
+ * This is the main test_schedule program file.
+ */
+
+#include <stdio.h>
+#include <propeller.h>
+#include <Pcon.h>
+
+
+int main(void)
+{
+
+	printf("hi there\n");
+  	return 0;
+}
+
